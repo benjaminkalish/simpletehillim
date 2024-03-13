3fad739eb4ca9a63feedd1f18091a473f1d24c7f
bkalish <benjamin.kalish@gmail.com>
2024-03-13 00:51:16 -0400

    hebcal

--- simple-tehilim/src/components/Main.js
+++ simple-tehilim/src/components/Main.js
@@ -1,28 +1,35 @@
 import React, { useState } from 'react'
 import lamnatzeach from '../lamnatzeach.jpeg'
 import '../css/Main.css'
-// import PerekSelect from './PerekSelect'
+import { HDate, gematriya } from '@hebcal/core'
 
 export default function Main() {
 
     const [perakimShown, setPerakimShown] = useState(false)
     const [sefarimShown, setSefarimShown] = useState(false)
 
-    const perakim = new Array(150).fill(0).map((x, i) => x = <button>{i + 1}</button>)
+    const perakim = new Array(150).fill(0).map((x, i) => x = <button>{gematriya(i + 1)}</button>)
 
-    const sefarim = new Array(5).fill(0).map((x, i) => x = <button id='i+1'>Sefer {i + 1}</button>)
+    const sefarim = new Array(5).fill(0).map((x, i) => x = <button id='i+1'>ספר {gematriya(i + 1)}</button>)
 
+    const date = new Date()
+
+    if(date.getHours() > 20){
+        date.setDate(date.getDate() + 1)
+    }
+
+    const hebDay = new HDate(date)
 
     return (
         <main id='home'>
             <div>
                 <h1>Simple Tehilim</h1>
-                <h3>Please don't launch the missiles.</h3>
+                <h3>Clear. Readable. Usable.</h3>
                 <div id='buttonContainer'>
-                    <button onClick={() => {setPerakimShown(true); setSefarimShown(false);}}>Select Perek</button>
-                    <button onClick={() => {setPerakimShown(false); setSefarimShown(true);}}>Select Sefer</button>
-                    <button>Launch the Missiles</button>
-                    <button>Reformat Hard drive</button>
+                    <button onClick={() => { setPerakimShown(true); setSefarimShown(false); }}>Select Perek</button>
+                    <button onClick={() => { setPerakimShown(false); setSefarimShown(true); }}>Select Sefer</button>
+                    <button>Tehilim for {hebDay.renderGematriya().split(' ').slice(0, -1).join(' ')}</button>
+                    <button>Tehilim for יום {gematriya(date.getDay() + 1)}</button>
                 </div>
             </div>
             <section>
