d287523357fc8f274f246b15775c78d9f80b0493
bkalish <benjamin.kalish@gmail.com>
2024-03-14 00:47:50 -0400

    reader init - fonts added

--- simple-tehilim/src/components/Reader.js
+++ simple-tehilim/src/components/Reader.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+import '../css/Reader.css'
 
 export default function Reader() {
+  const [data, setData] = useState(useLoaderData());
+  console.log(data)
+
   return (
-    <div>Reader</div>
+    <div id='readerContainer'>
+      <button id='menu'>☰</button>
+      <div id='textContainer'>{data.text.join('  ')}</div>
+    </div>
   )
 }
