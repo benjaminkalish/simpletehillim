5f6e83cc725aaeab0d575fa6fd4ae14bfde3eb5b
bkalish <benjamin.kalish@gmail.com>
2024-04-03 14:27:01 -0400

    perek headings added

--- simple-tehilim/src/components/Text.js
+++ simple-tehilim/src/components/Text.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect, useLayoutEffect, useCallback } from 'react'
-// import { gematriya } from '@hebcal/core';
+import { gematriya } from '@hebcal/core';
 
-export default function Text(props) {
+export default function Text({text}) {
     const [visibleText, setVisibleText] = useState([0, 0])
     const textContainerRef = useRef()
     const innerTextContainerRef = useRef()
@@ -9,16 +9,21 @@ export default function Text(props) {
     const topIndex = useRef(0)
     const bottomIndex = useRef(0)
 
-    // const text = props.text.map((datum, i) => <div className='perek' key={i}><h2 className='perekHeading'>{perekName(datum.perek)}</h2><p>{datum.text.join('  ')}</p></div>)
-    /* function perekName(n) {
+    // const testText = text.map((datum, i) => <div className='perek' key={i}><h2 className='perekHeading'>{perekName(datum.perek)}</h2><p>{datum.text.join('  ')}</p></div>)
+    function perekName(n) {
         return `פרק ${gematriya(n).replaceAll('׳', '')}`
-    } */
-    const testText = props.text.map((datum) => datum.text.join(' ')).join(' ').split(' ')
+    }
+    // const testText = text.map((datum) => datum.text.join(' ')).join(' ').split(' ')
+    const testText = text.flatMap(datum => [<h2 className='perekHeading'>{perekName(datum.perek)}</h2>].concat(datum.text.join(' ').split(' '))).map(x => typeof(x) === 'string' ? x + ' ' : x)
+    // const testText = text.map(datum => <div>datum.perek</div>)
+
+
     const didMount = useRef(false)
     //   const textRef = useRef()
     //   const [foo, setFoo] = useState(0)
 
     const layoutInitialize = useCallback(() => {
+        console.log(testText)
         bottomIndex.current = 70
         setVisibleText([topIndex.current, bottomIndex.current])
     }, [])
@@ -35,8 +40,6 @@ export default function Text(props) {
         if (!innerTextContainerRef || !textContainerRef) {
             return
         }
-        console.log(innerTextContainerRef.current)
-
         if (isRoom.current && textContainerRef.current.clientHeight < innerTextContainerRef.current.clientHeight) {
             bottomIndex.current = bottomIndex.current - 2
             setVisibleText([topIndex.current, bottomIndex.current])
@@ -70,7 +73,7 @@ export default function Text(props) {
     return (
         <div id='textContainer' /* onScroll={e => e.target.scrollTo(0, 0)} */ ref={textContainerRef}>
             <div id='innerTextContainer' ref={innerTextContainerRef} style={{ /* columnWidth: dimensions.width, left: left *//* , fontSize: dimensions.fontSize *//* dimensions.width / 14 + 'px' , *//* fontFamily: font */ }}>
-                {testText.slice(visibleText[0], visibleText[1]).join(' ')}
+                {testText.slice(visibleText[0], visibleText[1])}
             </div>
         </div>
     )
