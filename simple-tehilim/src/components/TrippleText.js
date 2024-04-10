c59be813585eba13799db046ab1f29e0c64f2c5f
bkalish <benjamin.kalish@gmail.com>
2024-04-09 22:22:58 -0400

    forward initial implementation

--- simple-tehilim/src/components/TrippleText.js
+++ simple-tehilim/src/components/TrippleText.js
@@ -1,14 +1,14 @@
-import React, { useRef, useState, useEffect, useLayoutEffect, useCallback } from 'react'
+import React, { useRef, useState, useEffect, useCallback } from 'react'
 import { gematriya } from '@hebcal/core';
 import PropTypes from 'prop-types';
 import '../css/TrippleText.css'
 
 export default function TrippleText({ text, font }) {
-    const [totalText, setTotalText] = useState(
+    const [fullText/* , setFullText */] = useState(
         text.flatMap(datum => [<h2 className='perekHeading' key={datum.perek}>{perekName(datum.perek)}</h2>]
             .concat(datum.text.join(' ').split(' ').map(x => x + ' ')))/* .map(x => typeof(x) === 'string' ? x + ' ' : x) */
     )
-    const [opacities, setOpacities] = useState({ a: 1, b: 0, c: 0 })
+    const [opacities, setOpacities] = useState({ a: 0, b: 0, c: 0 })
     const textContainerRef = useRef()
     const innerTextContainerRef = useRef()
     const page = useRef(1)
@@ -20,28 +20,28 @@ export default function TrippleText({ text, font }) {
     const p1IsSet = useRef(false)
     const p2IsSet = useRef(false)
     const p3IsSet = useRef(false)
-    const topIndex = useRef(0)
-    const bottomIndex = useRef(0)
+    /* const topIndex = useRef(0)
+    const bottomIndex = useRef(0) */
     const a = useRef()
     const b = useRef()
     const c = useRef()
-    
+
 
     function perekName(n) {
         return `פרק ${gematriya(n).replaceAll('׳', '')}`
     }
 
     const text1 = useCallback(() => {
-        return totalText.slice(top1.current, bottom1.current)
-    }, [totalText])
+        return fullText.slice(top1.current, bottom1.current)
+    }, [fullText])
 
     const text2 = useCallback(() => {
-        return totalText.slice(bottom1.current, bottom2.current)
-    }, [totalText])
+        return fullText.slice(bottom1.current, bottom2.current)
+    }, [fullText])
 
     const text3 = useCallback(() => {
-        return totalText.slice(bottom2.current, bottom3.current)
-    }, [totalText])
+        return fullText.slice(bottom2.current, bottom3.current)
+    }, [fullText])
 
     const [visibleText, setVisibleText] = useState({})
 
@@ -55,31 +55,31 @@ export default function TrippleText({ text, font }) {
 
     useEffect(() => {
         setVisibleText({ aText: text1(), bText: text2(), cText: text3() })
-    }, [text1, text2, text3, totalText])
+    }, [text1, text2, text3, fullText])
 
     const setPage3 = useCallback((element) => {
         if (textContainerRef.current.clientHeight
             < element.current.clientHeight) {
             bottom3.current = bottom3.current - 2
-            setVisibleText({ ...visibleText, cText: text3() })
+
         }
         else {
             p3IsSet.current = true
         }
-    }, [text3, visibleText])
+    }, [])
 
     const setPage2 = useCallback((element) => {
         if (textContainerRef.current.clientHeight
             < element.current.clientHeight) {
             bottom2.current = bottom2.current - 2
             bottom3.current = bottom2.current + maxWords
-            setVisibleText({ ...visibleText, bText: text2(), cText: text3() })
+
         }
         else {
             p2IsSet.current = true
             setPage3(c)
         }
-    }, [setPage3, text2, text3, visibleText])
+    }, [setPage3])
 
     const setPage1 = useCallback((element) => {
         if (textContainerRef.current.clientHeight
@@ -87,13 +87,14 @@ export default function TrippleText({ text, font }) {
             bottom1.current = bottom1.current - 2
             bottom2.current = bottom1.current + maxWords
             bottom3.current = bottom2.current + maxWords
-            setVisibleText({ aText: text1(), bText: text2(), cText: text3() })
+
         }
         else {
+            showPage()
             p1IsSet.current = true
             setPage2(b)
         }
-    }, [setPage2, text1, text2, text3])
+    }, [setPage2])
 
     useEffect(() => {
         if (!didMount.current) {
@@ -103,18 +104,21 @@ export default function TrippleText({ text, font }) {
 
         if (!p1IsSet.current) {
             setPage1(a)
+            setVisibleText({ aText: text1(), bText: text2(), cText: text3() })
         }
         else if (!p2IsSet.current) {
             setPage2(b)
+            setVisibleText({ ...visibleText, bText: text2(), cText: text3() })
         }
         else if (!p3IsSet.current) {
             setPage3(c)
+            setVisibleText({ ...visibleText, cText: text3() })
         }
         else {
             return
         }
 
-    }, [setPage1, setPage2, setPage3, visibleText])
+    }, [setPage1, setPage2, setPage3, text1, text2, text3, visibleText])
 
     /* const layoutRecalculate = useCallback(() => {
         if (!didMount.current) {
@@ -140,17 +144,51 @@ export default function TrippleText({ text, font }) {
          }
      }, [setVisibleText]) */
 
+    function showPage() {
+        const p = page.current
 
+        if (p % 3 === 0) {
+            setOpacities({ a: 0, b: 0, c: 1 })
+        }
+        else if (p % 3 === 2) {
+            setOpacities({ a: 0, b: 1, c: 0 })
+        }
+        else {
+            setOpacities({ a: 1, b: 0, c: 0 })
+        }
+    }
 
-
-    useLayoutEffect(() => {
+    useEffect(() => {
         function pageForward() {
-            if (bottomIndex.current >= totalText.length) {
+
+            /*  if (bottomIndex.current >= fullText.length) {
+                 return
+             }
+             topIndex.current = bottomIndex.current
+             bottomIndex.current = bottomIndex.current + 120
+             setVisibleText([topIndex.current, bottomIndex.current]) */
+            page.current = page.current + 1
+            showPage()
+
+            if (top1.current === 0 && page.current === 2) {
                 return
             }
-            topIndex.current = bottomIndex.current
-            bottomIndex.current = bottomIndex.current + 120
-            setVisibleText([topIndex.current, bottomIndex.current])
+
+            bottom2.current = bottom3.current
+            bottom3.current = bottom3.current + maxWords
+
+            if (page.current % 3 === 0) {
+                // setPage3(a)
+                setVisibleText({ ...visibleText, aText: text3() })
+            }
+            else if (page.current % 3 === 2) {
+                // setPage3(c)
+                setVisibleText({ ...visibleText, cText: text3() })
+            }
+            else {
+                // setPage3(b)
+                setVisibleText({ ...visibleText, bText: text3() })
+            }
         }
 
         function pageBack() {
@@ -193,7 +231,7 @@ export default function TrippleText({ text, font }) {
             // window.removeEventListener('resize', layoutInitialize)
             window.removeEventListener('click', clickHandler)
         }
-    }, [totalText.length, /* layoutInitialize */])
+    }, [text3, visibleText])
 
     return (
         <div id='textContainer' /* onScroll={e => e.target.scrollTo(0, 0)} */ ref={textContainerRef} style={{ fontFamily: font }}>
