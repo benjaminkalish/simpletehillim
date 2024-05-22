b87bc4a4c10b352c7beeaa2ddfbbba49eab048ed
bkalish <benjamin.kalish@gmail.com>
2024-05-22 03:30:57 -0400

    more typo fixes

--- tehillim_react/src/components/Main.js
+++ tehillim_react/src/components/Main.js
@@ -36,8 +36,8 @@ export default function Main() {
                 <div id='buttonContainer'>
                     <button onClick={() => { setPerakimShown(true); setSefarimShown(false); }}>Select Perek</button>
                     <button onClick={() => { setPerakimShown(false); setSefarimShown(true); }}>Select Sefer</button>
-                    <a className='buttonLink' href={`/month/${hebDay.getDate()}`} onClick={(e) => navigateTo(e, `/month/${hebDay.getDate()}`)}><button>Tehilim for {hebDay.renderGematriya().split(' ').slice(0, -1).join(' ')/* .replaceAll('׳', '') */}</button></a>
-                    <a className='buttonLink' href={`/week/${date.getDay() + 1}`} onClick={(e) => navigateTo(e, `/week/${date.getDay() + 1}`)}><button>Tehilim for יום {gematriya(date.getDay() + 1)}</button></a>
+                    <a className='buttonLink' href={`/month/${hebDay.getDate()}`} onClick={(e) => navigateTo(e, `/month/${hebDay.getDate()}`)}><button>Tehillim for {hebDay.renderGematriya().split(' ').slice(0, -1).join(' ')/* .replaceAll('׳', '') */}</button></a>
+                    <a className='buttonLink' href={`/week/${date.getDay() + 1}`} onClick={(e) => navigateTo(e, `/week/${date.getDay() + 1}`)}><button>Tehillim for יום {gematriya(date.getDay() + 1)}</button></a>
                 </div>
                 <small></small>
             </div>
