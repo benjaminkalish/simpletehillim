474e2b0e74c5c849feab7427e517c8b6153c9e0d
bkalish <benjamin.kalish@gmail.com>
2024-05-23 11:33:07 -0400

    help text update

--- tehillim_react/src/components/Help.js
+++ tehillim_react/src/components/Help.js
@@ -7,7 +7,7 @@ export default function Help({ setHelpShown }) {
             <div id='helpBox'>
                 <section>
                 <p>
-                    To go forward one page, left-click, scroll down or press left arrow, down arrow, or page down.
+                    To go forward one page, left-click, scroll down or press enter, space, left arrow, down arrow, or page down.
                 </p>
                 <p>
                     To go back one page, right-click, scroll up or press right arrow, up arrow, or page up.
