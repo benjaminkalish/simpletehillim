02c7a3f74f86dfd456ce81638940e7c00c9d5b31
kalishTAT <ben@torahanytime.com>
2024-08-01 00:52:23 -0400

    chore: help update for touch

--- tehillim_react/src/components/Help.js
+++ tehillim_react/src/components/Help.js
@@ -6,6 +6,7 @@ export default function Help({ setHelpShown }) {
     <div id="modal">
       <div id="helpBox">
         <section>
+          <h4>Desktop</h4>
           <p>
             To go forward one page, left-click, scroll down or press enter,
             space, left arrow, down arrow, or page down.
@@ -14,6 +15,11 @@ export default function Help({ setHelpShown }) {
             To go back one page, right-click, scroll up or press right arrow, up
             arrow, or page up.
           </p>
+        </section>
+        <section>
+          <h4>Mobile</h4>
+          <p>To go forward one page, tap, swipe up, or swipe right.</p>
+          <p>To go back one page, two-finger tap, swipe down, or swipe left.</p>
         </section>
         <button onClick={() => setHelpShown((h) => !h)}>Got it!</button>
       </div>
